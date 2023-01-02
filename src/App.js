5ef72b8b15c1290372416bbdad044adf93f86447
bpark <byungsun1819@gmail.com>
2023-01-02 05:01:37 -0800

    jsx updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, {Component} from "react";
 import "./App.css"
 
+
+
+
 export default class App extends Component {
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50px",
+    cursor: "pointer",
+    float: "right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: 'none'
+
+    }
+  }
+
   render() {
     return(
       <div className="container">
         <div className="todo-block">
           <div className="title">
-            To-Do List
+            <h1>To-Do List</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false}/>
+            Study
+            <button style={this.btnStyle}>x</button>
           </div>
 
         </div>
