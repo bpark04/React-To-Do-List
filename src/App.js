3187df95d751a945e60e6e700963dcc02ea24c2b
bpark <byungsun1819@gmail.com>
2023-01-05 03:34:02 -0800

    List component is created

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React, {useState} from "react";
-import "./App.css"
+import "./App.css";
+import List from "./Components/List.js";
 
 export default function App()  {
 
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50px",
-    cursor: "pointer",
-    float: "right"
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed? "line-through" : "none",
-    }
-  }
     
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(newTodoData);
-  };
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -48,30 +28,14 @@ export default function App()  {
     setValue("");
   };
 
-  const handleChangeCompleted = (id) => {
-    let newTodoData = todoData.map(data => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    })
-
-    setTodoData(newTodoData);
-  };
-
   return (
     <div className="container">
       <div className="todo-block">
         <div className="title">
           <h1>To-Do List</h1>
         </div>
-        {todoData.map(data => (
-          <div style={getStyle(data.completed)} key={data.id}>
-            <input type="checkbox" defaultChecked={false} onChange={() => handleChangeCompleted(data.id)}/>
-              {data.title}
-            <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-          </div>
-        ))}
+
+        <List todoData={todoData} setTodoData={setTodoData}/>
 
         <form style={{display: "flex"}} onSubmit={handleSubmit}>
           <input 
@@ -92,7 +56,4 @@ export default function App()  {
       </div>
     </div>
   )
-  
-    
-
 };
